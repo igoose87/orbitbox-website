acf40d841db0a944ac8c3be8bd611395f0bfa800
igoose87 <307518@niuitmo.ru>
2023-11-30 11:48:15 +0300

    feat: lab-5

--- js/localstorage-util.js
+++ js/localstorage-util.js
@@ -1,35 +1,13 @@
-let storage = window.localStorage;
-
-let submitAddress = document.getElementById("submit-address");
-submitAddress.addEventListener('click', addAddress);
-let addressesContent = document.getElementById("addresses-content");
-if (storage.getItem("address-name") == null){
-  addressesContent.innerText = ``
+let storage = localStorage;
+let greetingText = document.getElementById("greeting-text");
+if (storage !== null) {
+  greetingText.innerHTML = 
+  `
+    Welcome back, ${storage.getItem("name")}!
+  `
 } else {
-  addressesContent.innerText = 
-  ` 
-    ${storage.getItem("address-name")}
-    ${storage.getItem("address")}
-    ${storage.getItem("ship-method")}
+  greetingText.innerHTML = 
+  `
+    Welcome back, guest!
   `
 }
-
-let removeAddress = document.getElementById("delete-address");
-deleteAddress.addEventListener('click', deleteAddress);
-addressesContent.innerText = ``
-
-
-function addAddress() {
-  let addressName = document.getElementById("address-name");
-  let address = document.getElementById("address");
-  let shipMethod = document.getElementById("ship-method");
-  storage.setItem("address-name", addressName.value);
-  storage.setItem("address", address.value);
-  storage.setItem("ship-method", shipMethod.value);
- }
-
- function deleteAddress() {
-  storage.removeItem("address-name");
-  storage.removeItem("address");
-  storage.removeItem("ship-method");
- }
